3bf0b11cb49d32e7409516a24e43c0e48514ab00
zyq5428 <594270026@qq.com>
2017-12-13 15:58:15 +0800

    add key test application

---
+++ Drivers/part1/lesson6/key_app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int fd;
+	int key_num;
+	
+	/*open device*/
+	fd = open("/dev/ok6410key", 0);
+	
+	if (fd < 0)
+		printf("open device fail!\n");
+	
+	/*read device*/
+	read(fd, &key_num, 4);
+	printf("key is %d\n", key_num);
+
+	/*close device*/
+	close(fd);
+}
